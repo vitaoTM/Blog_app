29c3b3d4c88c3e2627087128d1078f29c5a04035
vitor <vitor.grosskopf@gmail.com>
2023-03-11 16:14:15 -0300

    Added new file, articles models, and new routes added

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,9 @@
+class PagesController < ApplicationController
+
+  def home
+  end
+
+  def home
+  end
+
+end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ApplicationRecord
-
+  validades :title, pesence: true, lenght: { minimun: 6, maximun: 50 }
+  validades :description, presence: true lenght: { minimun: 6, maximun: 200 }
 end
