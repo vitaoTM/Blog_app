fca9d4466b00c0c376da6a2e10a64a723aae2580
vitor <vitor.grosskopf@gmail.com>
2023-03-14 12:09:05 -0300

    remove cede redundancy, DRY

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only:[:show, :edit, :update, :destroy]
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -13,11 +13,10 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article created! yahhhs!!"
       redirect_to article_path(@article)
@@ -28,7 +27,7 @@ class ArticlesController < ApplicationController
 
   def update
     @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article Updated!"
       redirect_to @article
     else
@@ -37,9 +36,19 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+
+    params.require(:article).permit(:title, :description)
   end
 
 end
