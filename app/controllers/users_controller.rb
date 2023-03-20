97ae7a1553f632e82551181f603cbbd71b56846c
vitor <vitor.grosskopf@gmail.com>
2023-03-20 11:38:59 -0300

    Add index action to users_controller, to list all users using the application, * Add index.html page * Add link to Blogger in Navigation partial and leads to users index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,10 @@ class UsersController < ApplicationController
     @articles = @user.articles
   end
 
+  def index
+    @users = User.all
+  end
+
   def edit
     @user = User.find(params[:id])
   end
