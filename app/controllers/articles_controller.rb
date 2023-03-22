9c6aa9db35e6c539facba3a6dabad91039dad7c4
vitor <vitor.grosskopf@gmail.com>
2023-03-22 17:21:34 -0300

    Add require user method in application_record * add require same user private method in articles_controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only:[:show, :edit, :update, :destroy]
+  before_action :require_user, except:[:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def show
   end
@@ -48,8 +50,13 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-
     params.require(:article).permit(:title, :description)
   end
+
+  def require_same_user
+    if current_user != @article.user
+      flash[:alert] = "You can only modify your own article"
+      redirect_to @article
+    end
 
 end
