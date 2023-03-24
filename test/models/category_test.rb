058e8db6e1668cdd001d3dd4815874fd84bd7239
vitor <vitor.grosskopf@gmail.com>
2023-03-24 13:38:22 -0300

    Add unit tests for various category model validations specs * Add validations to the category model to make the tests pass

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -2,9 +2,36 @@ require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
 
-  test "category should be valid" do
+  def setup
     @category = Category.new(name: "Sports")
+  end
+
+  test "category should be valid" do
     assert @category.valid?
   end
+
+  test "name shoud be present" do
+    @category.name = " "
+    assert_not @category.valid?
+  end
+
+  test "name should be unique" do
+    @category.save
+    @category2 = Category.new
+    assert_not @category2.valid?
+  end
+
+  test "name should not be too long" do
+    @category.name = "a" * 26
+    assert_not @category.valid?
+  end
+
+  test "name should not be too short" do
+    @category.name = "aa"
+    assert_not @category.valid?
+  end
+
+
+
 
 end
