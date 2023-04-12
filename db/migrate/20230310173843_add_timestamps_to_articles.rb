2426d84c87560ab2c1ad31a7cc9bd6011a277b4e
vitor <vitor.grosskopf@gmail.com>
2023-04-11 22:36:25 -0300

    Update migration

--- db/migrate/20230310173843_add_timestamps_to_articles.rb
+++ db/migrate/20230310173843_add_timestamps_to_articles.rb
@@ -1,6 +1,6 @@
 class AddTimestampsToArticles < ActiveRecord::Migration[6.0]
   def change
-    add_column :articles, :created_at, :date_time
-    add_column :articles, :updated_at, :date_time
+    add_column :articles, :created_at, :timestamp
+    add_column :articles, :updated_at, :timestamp
   end
 end
